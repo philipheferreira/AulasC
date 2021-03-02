a85290ed92280ffc37a6b0ffd940eb9078e2842c
philipheferreira <philipheferreira@gmail.com>
2021-03-02 20:19:36 -0300

    atualização da aula 5 e 6

--- aula 5 - if complexo.cpp
+++ aula 5 - if complexo.cpp
@@ -20,15 +20,15 @@ main(){
 	
 		printf(" x é igual a 3\n");
 		
-	}else if((x >= 4 ) && (x < 10)){
+	}else if((x >= 4 ) && (x < 10)){ // o else deve vir antes do if para separar cada uma das situações. Caso o else não seja posicionado o codigo não funcionara de forma correta
 		
 		printf("x é maior ou igual a 4 e menor que 10\n"); 
 		
-	}else if((x == 11) || ( x == 14)){ 
+	}else if((x == 11) || ( x == 14)){ // dessa forma se pode colocar quantos ifs forem necessarios para o funcionamento da operação
 	
 		printf("o x é o 11 ou o 14\n");
 		
-	}else{
+	}else{ // o comando else é para todas as situações que não forem contempladas pelos ifs já definidos 
 		
 		printf("x não é nenhuma das opções anteriores\n");
 		
