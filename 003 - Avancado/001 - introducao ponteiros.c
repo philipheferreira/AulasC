95372e01a54ec4070ffaf216992872a3e768e5b6
philipheferreira <philipheferreira@gmail.com>
2026-05-15 20:01:28 -0300

    Corrections

---
+++ 003 - Avancado/001 - introducao ponteiros.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+
+/* Em tese o processo de ponteiro ja ocorre naturalmente mas sem fazermos
+nada. Quando criamos uma variavel nos alocamos um tipo de enderecamento
+dentro da memoria pra ela ser armazenada com o scanf, mas agora
+iremos brincar, mais pra frente com essa caracteristica*/
+
+
+int main(){ 
+	setlocale(LC_ALL, "");
+	
+	int x;
+	x = 10;
+	
+	int *ponteiro;
+	
+	printf("valor armazenado dentro da variavel: %i\n", x);
+	
+	printf("valor do endereço de memoria alocado para o armazenamento da variavel x: %i\n", &x);
+	
+	system("pause");
+}
