893f7638176c15d08377bbeb82bc5b2584fe277a
philipheferreira <philipheferreira@gmail.com>
2026-05-16 14:37:16 -0300

    Feat: ponteiros aula

---
+++ 003 - Avancado/005 - ponteiros char, int, double.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+
+
+
+
+int main(){ 
+	setlocale(LC_ALL, "");
+	
+	int valorInteiro = 10;
+	double valorDouble = 20.50;
+	char valorCaracter = 'a';
+	
+	
+	
+	system("pause");
+}
