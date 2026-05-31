69d6fa1bbce319f7c7b29243e2bd43540bf390f5
philipheferreira <philipheferreira@gmail.com>
2026-05-31 19:33:52 -0300

    Feat: create somethings

---
+++ 000 - projetos praticos/005 - to do list/001 - organizacao inicial de menu.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    int menu;
+	
+	do{
+		printf("\n=== TODO LIST ===\n");
+	    printf("1. Adicionar Tarefa\n");
+	    printf("2. Listar Tarefas\n");
+	    printf("3. Marcar como Concluida\n");
+	    printf("4. Deletar Tarefa\n");
+	    printf("5. Sair\n");
+	    printf("Escolha uma opcao: ");
+	    scanf("%d", &menu);
+	    if(menu == 1){
+			
+		}else
+		if(menu == 2){
+			
+		}else
+		if(menu == 3){
+			
+		}else
+		if(menu == 4){
+			
+		}else
+		if(menu == 5){
+			
+		}else{
+			printf("Voce nao escolheu nenhuma opcao anterior. escolha uma opcao do menu.");
+		}
+	}while( menu != 5);
+        
+
+}
+
